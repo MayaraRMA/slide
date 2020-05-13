83130bfe195340fb132be74dd712c61b293f6dec
Mayara Alencar <mayara.rma@gmail.com>
2020-05-13 19:50:47 -0300

    ajuste ao resize da pagina

--- js/slide.js
+++ js/slide.js
@@ -1,3 +1,5 @@
+import debounce from './debounce.js'
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
@@ -7,6 +9,7 @@ export default class Slide {
       startX: 0,
       movement: 0
     }
+    this.activeClass = 'active';
   }
 
   transition(active) {
@@ -74,11 +77,6 @@ export default class Slide {
     this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
 
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
@@ -106,9 +104,14 @@ export default class Slide {
 
   changeSlide(index) {
     const activeSlide = this.slideArray[index];
-    this.moveSlide(this.slideArray(index), position);
+    this.moveSlide(activeSlide, position);
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
+  }
+
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass)
   }
 
   activePrevSlide() {
@@ -119,11 +122,30 @@ export default class Slide {
     if (this.index.next !== undefined) this.changeSlide(this.index.next)
   }
 
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize)
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.addResizeEvent = debounce(this.addResizeEvent.bind(this), 200);
+  }
+
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
-    this.transition(true);
+    this.addResizeEvent();
     return this;
   }
 }
